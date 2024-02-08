5d9d419b0b2f5fd00129b2d6575e8a76a365818e
Jhicken16 <jhicken16@gmail.com>
2024-02-08 16:53:59 +0000

    Strict mode removed due to useEffect rendering multiple time and creating error in the updates of transcript. useTranscript hook works by updating text with transcript saving index of last finalTranscripty that was added. When final transcript is avalable remove transcript replacing with final to persereve accrecy.

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect}from 'react'
+import React, {useState, useEffect, useRef}from 'react'
 
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
@@ -12,9 +12,10 @@ function App() {
     resetTranscript,
     browserSupportsSpeechRecognition,
     interimTranscript,
+    finalTranscript,
   } = useSpeechRecognition()
 
-  const [text, changeState] = useTranscript(transcript, resetTranscript, interimTranscript)
+  const [text, changeState] = useTranscript(transcript, resetTranscript, finalTranscript)
 
   
 
@@ -25,7 +26,10 @@ function App() {
     <>
     <div>
       <h3>Mic: {listening ? 'on' : 'off'}</h3>
-      <button onClick={SpeechRecognition.startListening}>on</button>
+      <button onClick={() => {
+        changeState(0)
+        SpeechRecognition.startListening({continuous: true})
+      }}>on</button>
       <button onClick={SpeechRecognition.stopListening}>off</button>
       <p>{text[0]}</p>
     </div>
@@ -46,30 +50,81 @@ function App() {
 
 export default App;
 
-function useTranscript(transcript, resetTranscript){
-  
+function useTranscript(transcript, resetTranscript, finalTranscript){
+ 
   const [i, setI] = useState(0)
 
   const [text, setText] = useState('')
   const [deleteLastWord, setDeleteLastWord] = useState('pop')
+  const transcriptLengthRef = useRef(0)
+  
+  const commands = (transcript) => {
+    console.log(transcript + 'delete')
+    return 'null'
+  }
+  
 
   const whichState = [setText, setDeleteLastWord]
 
   let callback = whichState[i]
-
+  
   useEffect(() => {
-    checkTrans(transcript)
-    callback(transcript)
-  }, [transcript, callback])
 
+//transcript = checkTrans(transcript, commands)
+
+    if(finalTranscript !== ''){
+
+      callback(prev => {
+        const toAdd = prev.substring(0, transcriptLengthRef.current) + ' ' + finalTranscript
+
+        transcriptLengthRef.current += finalTranscript.length+1
+
+        return toAdd
+      })
+      resetTranscript()
+      
+    }
+
+    else if(transcript !== ''){
+      callback(prev => {
+        if(prev.length){
+
+          return prev.substring(0, transcriptLengthRef.current) + ` ${transcript}`
+        }
+
+        return transcript
+      })
+    }
+
+  }, [transcript, callback, finalTranscript,resetTranscript])
+
+
+  //this function is exported from hook and is used to change which state is updated with the transcript.
   function changeState(x){
     setI(x)
   }
-
+  
   return [[text, deleteLastWord], changeState]
 }
 
-function checkTrans(transcript){
+
+function checkTrans(transcript, commands){
+  if (typeof transcript === 'undefined') {
+    // Handle the case when transcript is undefined
+    return;
+  }
+  console.log(transcript)
   const lastPosition = transcript.lastIndexOf(' ')
-  //console.log(transcript.substring(lastPosition + 1))
+  const lastWord = transcript.substring(lastPosition + 1)
+  console.log(lastWord)
+  if(lastWord !== ''){
+    var regex = /[A-Z]*pop[A-Z]*\W*/i;
+    if(regex.test(lastWord)){
+      return commands(transcript)
+    }
+    else {
+      return transcript
+    }
+  }
+  
 }
