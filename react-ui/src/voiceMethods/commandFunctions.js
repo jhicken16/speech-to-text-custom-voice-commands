c2a61096c84d470fb7708fde59784018f0302116
Jhicken16 <jhicken16@gmail.com>
2024-02-19 11:20:31 +0000

    react-ui/src/voiceMethods/commandFunctions.js new folder and file added. will contain functions that will be stored in hash to manipulate transcripted according to user.

---
+++ react-ui/src/voiceMethods/commandFunctions.js
@@ -0,0 +1,30 @@
+
+/*
+    This function are going to be interaction with the test useState. So they will be used in the set state function.
+    therefor function should return the hole state that has been changed. 
+*/
+
+
+function deleteMethod(frontIndex, backIndex, to, text){
+
+    //to skip space that is before command word.
+    frontIndex =- 2
+
+    // Make sure command was not the first word
+    if(frontIndex < 1){
+        return text
+    }
+
+    for(frontIndex; frontIndex > 0; frontIndex--){
+        if(text[frontIndex] === to){
+            const beforeCommand = text.subString(0, frontIndex)
+            const afterCommand = text.subSting(backIndex + 1)
+
+            return beforeCommand + afterCommand
+        }
+    }
+
+    // is set char was not found return text. Maybe return text with out command word.
+    return text
+
+}
