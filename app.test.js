bebe32bb207acbbfda19b5c66519db55fd02f3fb
Jhicken16 <jhicken16@gmail.com>
2024-03-18 13:22:41 +0000

    given email and password returns with status 200 test passed and commited to db.

---
+++ app.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest')
+const app = require('./app')
+
+describe("POST /register", () => {
+
+    describe("given email and password", () => {
+        it("return 200 status code", async () => {
+            const response = await request(app).post("/auth/register").send({
+                email: "testenv2@gmail.com",
+                password: "testenv"
+            })
+            expect(response.statusCode).toBe(200)
+
+        })
+        // it("return session coolie for authentication", () => {
+
+        // })
+    })
+})
